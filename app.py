dd43f9cdaa8976b320820f5212e310482acf2d7d
AmongSuslikeu <fteisaire.dev@gmail.com>
2026-01-06 05:49:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1182,7 +1182,7 @@ HTML_DATA = r"""
 
 port = int(os.environ.get("PORT", 5000))
 try:
-    ocketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
     logging.exception("Server is running")
 except Exception:
     logging.exception("Server crashed on run")
